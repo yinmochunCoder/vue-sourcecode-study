ea84dc80af604e45b043e12dd3a29dac54cdc9bc
肖艳丽 <01532410@yto.net.cn>
2020-11-27 16:11:59 +0800

    vue源码手写：定义数据监控

--- src/observe/index.js
+++ src/observe/index.js
@@ -9,21 +9,34 @@ export function observe(data) {
     // 非对象返回
     if (!isObject(data)) return
     //创建ob实例
-    var ob = new Observe()
+    var ob = new Observe(data)
 
-    let keys = Object.keys(data)
-    keys.forEach(key => {
-        Object.defineProperty(vm,key,{
-            get() {
-                return data[key]
-            },
-            set(newValue) {
-                data[key] = newValue
-            }
-        })
-    })
+
 }
 
-function Observe() {
+function Observe(value) {
+    this.value = value
+    this.walk(value)
+}
 
+Observe.prototype.walk = function(obj) {
+    let keys = Object.keys(obj)
+    for ( let key = 0; key < keys.length; key++ ) {
+        this.defineReactive(obj,keys[key])
+    }
+
+}
+
+Observe.prototype.defineReactive = function(obj,key) {
+    var val = obj[key]
+    observe(val)
+    Object.defineProperty(obj,key,{
+        get() {
+            var value = val
+            return value
+        },
+        set(newValue) {
+            obj[key] = newValue
+        }
+    })
 }
