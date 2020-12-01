78ea2b90372da4949a61766e7cedcca0178e75fb
肖艳丽 <01532410@yto.net.cn>
2020-12-01 17:57:46 +0800

    vue手写源码：数组下的对象监测

---
+++ src/observe/array.js
@@ -0,0 +1,41 @@
+export const arrayMethods = Object.create(Array.prototype)
+
+const methodsToPatch = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'reverse',
+    'splice',
+    'sort'
+]
+
+methodsToPatch.forEach(method => {
+    // 函数劫持，首先获取原始方法
+    var original = arrayMethods[method]
+    console.log(original,"original")
+    Object.defineProperty(arrayMethods,method, {
+        value: function mutator() {
+            // 获取传入参数
+            var args = [], length = arguments.length
+            while(length--) args[length] = arguments[length]
+            //　执行继承的原始方法，指定传入this值，参数
+            var result = original.apply(this,args),
+                insterd, ob = this.__ob__;
+            console.log(this,'arrayMethods下的this')
+            // 对新增的方法进行自定义
+            switch (method) {
+                case 'push':
+                case 'unshift':
+                    insterd = args
+                    break;
+                case 'splice':
+                    // 获取第三个值参数，监控第三个值
+                    insterd = args.slice(2)
+                    break
+            }
+            if(insterd) { ob.observeArray(insterd) }
+            return result
+        }
+    })
+})
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,4 +1,6 @@
 import { isObject } from '../utils/index'
+import { arrayMethods } from "./array";
+
 /**
  * 数据监测
  * @param data
@@ -7,13 +9,28 @@ import { isObject } from '../utils/index'
 export function observe(data) {
     // 非对象返回
     if (!isObject(data)) return
+
     // 创建Observe实例
     var ob = new Observe(data)
+    // 暴露__ob__给array
+    Object.defineProperty(data,'__ob__',{
+        value: ob
+    })
     if (Array.isArray(data)) {
-        
+        // 数组方法，从array的原型上继承原始方法
+        console.log(arrayMethods,"arrayMethods.__proto__")
+        // 重写array中的方法
+        protoAugment(data,arrayMethods)
+        // 监测数组
+        ob.observeArray(data)
     } else {
         ob.walk(data)
     }
+}
+// 继承并重写array数组原型方法
+function protoAugment(target,src) {
+    // 将data的proto属性指向重写后的数组方法所在的实例原型中
+    target.__proto__ = src
 }
 // class的写法
 class Observe {
@@ -21,10 +38,15 @@ class Observe {
         this.value = value
     }
     walk(obj) {
-        console.log(obj)
         let keys = Object.keys(obj)
         for ( let index = 0; index < keys.length; index++ ) {
             this.defineReactive(obj,keys[index])
+        }
+    }
+
+    observeArray(obj) {
+        for ( let index = 0; index < obj.length; index++ ) {
+            observe(obj[index]);
         }
     }
 
